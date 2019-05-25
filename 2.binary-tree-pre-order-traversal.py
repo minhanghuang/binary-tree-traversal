e087ab2e6d7afbc98f4f0194ee9c0402cd1c462f
Coxhuang <mhesat@outlook.com>
2019-05-25 13:23:16 +0800

    update

---
+++ 2.binary-tree-pre-order-traversal.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+class TreeNode:
+    """
+    节点
+    """
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def levelOrderBottom(self, root):
+        """
+        先序遍历
+        :param root: 根节点
+        :return: list_node -> List
+        """
+
+        stack_node = [root]  # 栈
+        list_node = []  # 先序遍历结果存放列表
+
+        while stack_node: # 栈不为空
+            node = stack_node.pop() # 栈顶节点出栈
+            if not node: # 节点为空
+                continue
+            list_node.append(node.val) # 把不为空的节点数值存到列表
+            stack_node.append(node.right) # 右节点先压栈
+            stack_node.append(node.left) # 左节点后压栈
+
+        print(list_node)
+
+        return list_node
+
+
---
+++ 3.binary-tree-in-order-traversal.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+class TreeNode:
+    """
+    节点
+    """
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def levelOrderBottom(self, root):
+        """
+        中序遍历 非递归
+        :param root:  根节点
+        :return: list_node -> List
+        """
+        stack_node = []  # 栈
+        list_node = []  # 中序遍历结果存放列表
+        node_p = root # 当前节点
+        while stack_node or node_p: # 当前节点不为空 or 栈不为空
+
+            while node_p: # 一直移动到最左端
+                stack_node.append(node_p) # 节点压栈
+                node_p = node_p.left # 指针左移
+
+            node = stack_node.pop() # 出栈
+            list_node.append(node.val) # 获取节点数据
+            node_p = node.right # 获取有节点
+
+        print(list_node)
+
+        return list_node
+
+
